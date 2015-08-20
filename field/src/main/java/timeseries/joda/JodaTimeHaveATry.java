83df5b69993bbc4de87b3d1c85cf625dd8f4d944
jinjy@zhong-ying.com <jinjy@zhong-ying.com>
2015-08-20 12:27:45 +0800

    1. have a try for joda; 2. convert project to utf8

--- field/src/main/java/timeseries/joda/JodaTimeHaveATry.java
+++ field/src/main/java/timeseries/joda/JodaTimeHaveATry.java
@@ -1,10 +1,47 @@
 package timeseries.joda;
 
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by asdf2014 on 2015/8/20.
  */
 public class JodaTimeHaveATry {
 
+    private static Date date;
+
+    @Before
+    public void before(){
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-hh:mm:ss");
+
+        try {
+            date = sdf.parse("2015-08-12-11:20:38");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Test
+    public void test() {
+
+        DateTime jodaTime = new DateTime(date);
+
+        assertEquals(2015, jodaTime.getYear());
+        assertEquals(20, jodaTime.getMinuteOfHour());
+
+        assertEquals("八月", jodaTime.monthOfYear().getAsShortText());
+
+    }
 
 
 }
