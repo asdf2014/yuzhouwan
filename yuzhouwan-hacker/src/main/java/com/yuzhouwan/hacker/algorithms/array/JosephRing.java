239e8977ffd23144f343538cf0a02eb7bb19db51
asdf2014 <1571805553@qq.com>
2016-08-31 18:00:37 +0800

    Joseph Ring

---
+++ yuzhouwan-hacker/src/main/java/com/yuzhouwan/hacker/algorithms/array/JosephRing.java
@@ -0,0 +1,32 @@
+package com.yuzhouwan.hacker.algorithms.array;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Copyright @ 2016 yuzhouwan.com
+ * All right reserved.
+ * Function: Joseph Ring
+ *
+ * @author Benedict Jin
+ * @since 2016/8/31
+ */
+public class JosephRing {
+
+    public List<Integer> joseph(List<Integer> origin, int countNum) {
+
+        List<Integer> result = new LinkedList<>();
+        int k = 0;
+        while (origin.size() > 0) {
+            k = k + countNum;
+            k = k % (origin.size()) - 1;
+            if (k == -1) {
+                result.add(origin.remove(origin.size() - 1)); //remove last elements
+                k = 0;
+            } else {
+                result.add(origin.remove(k));
+            }
+        }
+        return result;
+    }
+}
---
+++ yuzhouwan-hacker/src/test/java/com/yuzhouwan/hacker/algorithms/JosephRingTest.java
@@ -0,0 +1,31 @@
+package com.yuzhouwan.hacker.algorithms;
+
+import com.yuzhouwan.hacker.algorithms.array.JosephRing;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Copyright @ 2016 yuzhouwan.com
+ * All right reserved.
+ * Function: Joseph Ring Tester
+ *
+ * @author Benedict Jin
+ * @since 2016/8/31
+ */
+public class JosephRingTest {
+
+    @Test
+    public void joseph() throws Exception {
+        List<Integer> origin = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            origin.add(i);
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        new JosephRing().joseph(origin, 3).forEach(e -> stringBuilder.append(e + " "));
+        assertEquals("3 6 9 2 7 1 8 5 10 4", stringBuilder.toString().trim());
+    }
+}
