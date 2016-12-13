1c3ddd360b1a192380d0b5f6a133f22edd7c28c3
asdf2014 <1571805553@qq.com>
2016-12-13 11:01:49 +0800

    advance logback

--- yuzhouwan-hacker/src/main/java/com/yuzhouwan/hacker/algorithms/thread/feature/CallableFutureExample.java
+++ yuzhouwan-hacker/src/main/java/com/yuzhouwan/hacker/algorithms/thread/feature/CallableFutureExample.java
@@ -31,7 +31,7 @@ public class CallableFutureExample {
             );
             _log.info("waiting...");
             try {
-                _log.info(future.get());
+                _log.debug(future.get());
             } catch (InterruptedException | ExecutionException e) {
                 _log.error("error: {}", e.getMessage());
             }
@@ -50,7 +50,7 @@ public class CallableFutureExample {
         _log.info("All thread were submitted into pool...");
         for (int i = 0; i < 10; i++) {
             try {
-                _log.info("{}", completionService.take().get());
+                _log.debug("{}", completionService.take().get());
             } catch (InterruptedException | ExecutionException e) {
                 _log.error("error: {}", e.getMessage());
             }
