7404d2bcb475a43a08dc7580e485564d7f12322d
asdf2014 <1571805553@qq.com>
2017-04-18 17:06:40 +0800

    Expand FinallyTest

--- yuzhouwan-hacker/src/test/java/com/yuzhouwan/hacker/effective/FinallyTest.java
+++ yuzhouwan-hacker/src/test/java/com/yuzhouwan/hacker/effective/FinallyTest.java
@@ -1,18 +1,22 @@
 package com.yuzhouwan.hacker.effective;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static junit.framework.TestCase.assertEquals;
 
 /**
  * Copyright @ 2017 yuzhouwan
  * All right reserved.
- * Function：Finally Test
+ * Function：Test cases about Finally
  *
  * @author Benedict Jin
  * @since 2017/4/18
  */
 public class FinallyTest {
+
+    private static final Logger _log = LoggerFactory.getLogger(FinallyTest.class);
 
     @Test
     public void testReturnInside() throws Exception {
@@ -47,6 +51,69 @@ public class FinallyTest {
             throw new RuntimeException("Some Problem.");
         } catch (Exception e) {
             throw new RuntimeException(e);
+        } finally {
+        }
+    }
+
+    @Test
+    public void testReturnInside2() throws Exception {
+        /*
+        2017-04-18 16:52:59.234 | ERROR | Some Problem. | com.yuzhouwan.hacker.effective.FinallyTest.someExceptionWrong2 | FinallyTest.java:68
+        2017-04-18 16:52:59.253 | ERROR | Some Problem. | com.yuzhouwan.hacker.effective.FinallyTest.someExceptionRight2 | FinallyTest.java:78
+         */
+        someExceptionWrong2();
+        someExceptionRight2();
+    }
+
+    private void someExceptionWrong2() {
+        try {
+            throw new RuntimeException("Some Problem.");
+        } catch (Exception e) {
+            _log.error("{}", e.getMessage());
+        } finally {
+            return;
+        }
+    }
+
+    private void someExceptionRight2() {
+        try {
+            throw new RuntimeException("Some Problem.");
+        } catch (Exception e) {
+            _log.error("{}", e.getMessage());
+        } finally {
+        }
+    }
+
+    @Test
+    public void testReturnInside3() throws Exception {
+
+        Exception catchE = null;
+        try {
+            someExceptionWrong3();
+        } catch (Exception e) {
+            catchE = e;
+        }
+        assertEquals(true, catchE == null);
+
+        try {
+            someExceptionRight3();
+        } catch (Exception e) {
+            catchE = e;
+        }
+        assertEquals("Some Problem.", catchE.getMessage());
+    }
+
+    private void someExceptionWrong3() {
+        try {
+            throw new RuntimeException("Some Problem.");
+        } finally {
+            return;
+        }
+    }
+
+    private void someExceptionRight3() {
+        try {
+            throw new RuntimeException("Some Problem.");
         } finally {
         }
     }
