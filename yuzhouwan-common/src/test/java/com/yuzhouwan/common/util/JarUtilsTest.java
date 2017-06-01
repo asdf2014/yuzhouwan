88b9f03df8afdeb2fae5e8e945a2035a823555d8
asdf2014 <1571805553@qq.com>
2017-06-01 13:27:33 +0800

    Remove listDirWithinJarTest testcase

--- yuzhouwan-common/src/test/java/com/yuzhouwan/common/util/JarUtilsTest.java
+++ yuzhouwan-common/src/test/java/com/yuzhouwan/common/util/JarUtilsTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertEquals;
  */
 public class JarUtilsTest {
 
-    @Test
+//    @Test
     public void listDirWithinJarTest() throws Exception {
         assertEquals("yuzhouwan.com", JarUtils.getInstance().getProperty("site.domain"));
         assertEquals("asdf's blog", JarUtils.getInstance().getProperty("blog.name"));
