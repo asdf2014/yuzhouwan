549c9e2884b3be2bcd1b34209c2eecf98cc49506
asdf2014 <benedictjin2016@gmail.com>
2017-08-09 10:25:18 +0800

    Add JMH simple case

---
+++ yuzhouwan-bigdata/yuzhouwan-bigdata-zookeeper/src/test/java/com/yuzhouwan/bigdata/zookeeper/benchmark/BenchmarkSimple.java
@@ -0,0 +1,47 @@
+package com.yuzhouwan.bigdata.zookeeper.benchmark;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Copyright @ 2017 yuzhouwan.com
+ * All right reserved.
+ * Function：Benchmark Simple
+ *
+ * @author Benedict Jin
+ * @since 2017/8/9
+ */
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Thread)
+public class BenchmarkSimple {
+
+    @Benchmark
+    public void bench() {
+        add(1, 1);
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
+    }
+
+    /*
+    Benchmark               Mode  Cnt         Score        Error   Units
+    BenchmarkSimple.bench  thrpt    5  13352311.603 ± 767137.272  ops/ms
+     */
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(BenchmarkSimple.class.getSimpleName())
+                .forks(1)
+                .warmupIterations(5)
+                .measurementIterations(5)
+                .threads(10)
+                .build();
+        new Runner(opt).run();
+    }
+}
