4cedac566acd31f28a981b209c5699a91ae53ec0
asdf2014 <benedictjin2016@gmail.com>
2017-08-16 17:21:10 +0800

    Improve znode_count.sh

--- yuzhouwan-bigdata/yuzhouwan-bigdata-zookeeper/src/main/resources/shell/count_znode.sh
+++ yuzhouwan-bigdata/yuzhouwan-bigdata-zookeeper/src/main/resources/shell/count_znode.sh
@@ -15,41 +15,48 @@ if [ -z "$dataDir" -o -z "$zkHome" -o -z "$clientPort" -o -z "$znodeParentPath" 
     exit
 fi
 
-newest_snapshot=`ls -l "${dataDir}"/version-2/snapshot.* | awk 'END{print $9}'`
-# Newest snapshot: /home/zookeeper/data//version-2/snapshot.0
-echo "Newest snapshot: ${newest_snapshot}"
+build_newest_snapshot() {
+    newest_snapshot=`ls -l "${dataDir}"/version-2/snapshot.* | awk 'END{print $9}'`
+    # Newest snapshot: /home/zookeeper/data//version-2/snapshot.0
+    echo "Newest snapshot: ${newest_snapshot}"
 
-cd ${zkHome}
-mkdir -p ${tmpPath}
-tmp=${tmpPath}/snapshot.`date '+%Y%m%d%H%M%S'`
-# Tmp: /home/zookeeper/zk-monitor/snapshot/snapshot.20170816142407
-echo "Tmp: ${tmp}"
-java -cp zookeeper-3.4.6.jar:lib/log4j-1.2.16.jar:lib/slf4j-log4j12-1.6.1.jar:lib/slf4j-api-1.6.1.jar org.apache.zookeeper.server.SnapshotFormatter ${newest_snapshot} > ${tmp}
+    cd ${zkHome}
+    mkdir -p ${tmpPath}
+    tmp=${tmpPath}/snapshot.`date '+%Y%m%d%H%M%S'`
+    # Tmp: /home/zookeeper/zk-monitor/snapshot/snapshot.20170816142407
+    echo "Tmp: ${tmp}"
+    java -cp zookeeper-3.4.6.jar:lib/log4j-1.2.16.jar:lib/slf4j-log4j12-1.6.1.jar:lib/slf4j-api-1.6.1.jar org.apache.zookeeper.server.SnapshotFormatter ${newest_snapshot} > ${tmp}
+}
 
-arr=`echo "ls ${znodeParentPath}" | zkCli.sh -server localhost:${clientPort} | grep zookeeper`
-# [leader, election, zookeeper]
-echo -e "Origin:\n\t ${arr}\n"
+parse_newest_snapshot() {
+    arr=`echo "ls ${znodeParentPath}" | zkCli.sh -server localhost:${clientPort} | grep zookeeper`
+    # [leader, election, zookeeper]
+    echo -e "Origin:\n\t ${arr}\n"
 
-arr=`echo ${arr:1:${#arr}-2}`
-# leader, election, zookeeper
-echo -e "Cut head & tail:\n\t ${arr}\n"
+    arr=`echo ${arr:1:${#arr}-2}`
+    # leader, election, zookeeper
+    echo -e "Cut head & tail:\n\t ${arr}\n"
 
-OLD_IFS="$IFS"
-IFS=$", "
-arr=(${arr})
+    OLD_IFS="$IFS"
+    IFS=$", "
+    arr=(${arr})
 
-result=
-for a in ${arr[@]}; do
-    count=`cat ${tmp} | grep "/${a}" | wc -l`
-    result=`echo -e "\t\t${result}\n${count}\t/${a}"`
-done
+    result=
+    for a in ${arr[@]}; do
+        count=`cat ${tmp} | grep "/${a}" | wc -l`
+        result=`echo -e "\t\t${result}\n${count}\t/${a}"`
+    done
 
-# 0	/election
-# 0	/leader
-# 2	/zookeeper
-sorted=`echo ${result} | sort -k1 -n`
-topNInfo=`echo ${sorted} | tail -${topN}`
-echo -e "Top${topN}: \n${topNInfo}"
+    # 0	/election
+    # 0	/leader
+    # 2	/zookeeper
+    sorted=`echo ${result} | sort -k1 -n`
+    topNInfo=`echo ${sorted} | tail -${topN}`
+    echo -e "Top${topN}: \n${topNInfo}"
 
-IFS="$OLD_IFS"
-echo "Roll back Old IFS."
+    IFS="$OLD_IFS"
+    echo "Roll back Old IFS."
+}
+
+build_newest_snapshot
+parse_newest_snapshot
