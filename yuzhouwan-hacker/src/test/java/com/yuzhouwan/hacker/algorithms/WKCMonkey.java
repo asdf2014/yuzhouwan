a9d9ece382c68ac7b09f026cb9da3740b77f5e38
asdf2014 <benedictjin2016@gmail.com>
2018-01-11 21:33:05 +0800

    Improve WKCMonkey algorithm

--- yuzhouwan-hacker/src/test/java/com/yuzhouwan/hacker/algorithms/WKCMonkey.java
+++ yuzhouwan-hacker/src/test/java/com/yuzhouwan/hacker/algorithms/WKCMonkey.java
@@ -2,6 +2,8 @@ package com.yuzhouwan.hacker.algorithms;
 
 import org.junit.Test;
 
+import static com.yuzhouwan.common.util.StrUtils.isBlank;
+
 /**
  * Copyright @ 2018 yuzhouwan.com
  * All right reserved.
@@ -11,6 +13,11 @@ import org.junit.Test;
  * @since 2018/1/10
  */
 public class WKCMonkey {
+
+    private static final int MAX_FEED = 5;
+    private static final int MAX_COUNT = 10;
+    private static final String HIGH_SPLITTER = ",";
+    private static final String LOW_SPLITTER = " ";
 
     /*
         max: 0.99984, maxCount: 4.0, sum: 0.99984, count: 4.0
@@ -27,53 +34,52 @@ public class WKCMonkey {
     @Test
     public void bestChoice() {
         makeTheBestChoice("24996");
-        makeTheBestChoice("35340");
+        makeTheBestChoice("200004");
     }
 
     private void makeTheBestChoice(String monkeyId) {
-        Integer v = Integer.valueOf(monkeyId);
-        double feed = v / (Math.pow(10, monkeyId.length()));
+        double feed = Integer.valueOf(monkeyId) / (Math.pow(10, monkeyId.length()));
         double max = 0, sum = 0;
         int count = 0, maxCount = 0;
-        int pointBefore;
+        int pointBefore, maxPointBefore = 0;
         double pointAfter;
         String monkeyIdStr = feed + "";
         String monkeyIdPointAfterStr = monkeyIdStr.substring(1, monkeyIdStr.length());
-        while (sum < 5) {
+        while (sum < MAX_FEED) {
             count++;
-            if ((sum += feed) > 5) break;
+            if (count > MAX_COUNT || (sum += feed) > MAX_FEED) break;
             pointAfter = sum - (pointBefore = (int) sum);
             if (pointAfter > max) {
+                maxPointBefore = pointBefore;
                 max = pointAfter;
                 maxCount = count;
             }
             System.out.println(String.format("max: %s, maxCount: %s, sum: %s, count: %s", max, maxCount, sum, count));
             System.out.println(String.format("pointBefore: %s, pointAfter: %s\n", pointBefore, pointAfter));
         }
-        String result = theBest(4, maxCount, 0, new StringBuffer(), new StringBuffer()).toString();
-        String[] split = result.split(",");
+        String result = theBest(MAX_FEED - 1 - maxPointBefore, maxCount, 0, new StringBuffer(), new StringBuffer()).toString();
+        String[] split = result.split(HIGH_SPLITTER);
         for (String s : split) {
-            if (s == null || s.length() == 0) continue;
-            String[] single = s.split(" ");
-            for (String single_ : single) {
-                System.out.print(single_ + monkeyIdPointAfterStr + "\t\t");
+            if (isBlank(s)) continue;
+            String[] single = s.split(LOW_SPLITTER);
+            for (String _single : single) {
+                System.out.print(_single + monkeyIdPointAfterStr + "\t\t");
             }
             System.out.println();
         }
+        System.out.println("\n\n");
     }
 
     private static StringBuffer theBest(int n, int m, int start, StringBuffer path, StringBuffer result) {
-        if (n == 0) {
-            result.append(path).append(",");
-        }
+        if (n == 0) result.append(path).append(HIGH_SPLITTER);
         if (start <= n && m == 1) {
             path.append(n);
-            result.append(path).append(",");
+            result.append(path).append(HIGH_SPLITTER);
             path.setLength(path.length() - 1);
             return result;
         }
         for (int i = start; i <= n - 1; i++) {
-            theBest(n - i, m - 1, i, path.append(i).append(" "), result);
+            theBest(n - i, m - 1, i, path.append(i).append(LOW_SPLITTER), result);
             path.setLength(path.length() - 2);
         }
         return result;
