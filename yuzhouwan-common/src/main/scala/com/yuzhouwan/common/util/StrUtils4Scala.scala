5226046a84aeaf722f3770e0a0b0f58d6afaded0
asdf2014 <benedictjin2016@gmail.com>
2018-05-29 23:46:53 +0800

    Add String Utils for Scala

---
+++ yuzhouwan-common/src/main/scala/com/yuzhouwan/common/util/StrUtils4Scala.scala
@@ -0,0 +1,29 @@
+package com.yuzhouwan.common.util
+
+/**
+  * Copyright @ 2018 yuzhouwan.com
+  * All right reserved.
+  * Function：String Utils for Scala
+  *
+  * @author Benedict Jin
+  * @since 2018/5/30
+  */
+object StrUtils4Scala {
+
+  def countSubString(str: String, sub: String): Int = str.sliding(sub.length).count(slide => slide == sub)
+
+  def patchSubString(str: String, sub: String): String = str.patch(str.lastIndexOf(sub), "", sub.length)
+
+  def superFormat(str: String, detail: String*): String = {
+    val holderSize: Int = countSubString(str, "%s")
+    val detailSize: Int = detail.size
+
+    var result: String = str
+    if (detailSize < holderSize) {
+      for (_ <- 0 until (holderSize - detailSize)) {
+        result = patchSubString(result, "%s")
+      }
+    }
+    result.format(detail: _*)
+  }
+}
---
+++ yuzhouwan-common/src/test/scala/com/yuzhouwan/common/util/StrUtils4ScalaTest.scala
@@ -0,0 +1,35 @@
+package com.yuzhouwan.common.util
+
+import com.yuzhouwan.common.UnitTestStyle
+import com.yuzhouwan.common.util.StrUtils4Scala._
+
+/**
+  * Copyright @ 2018 yuzhouwan.com
+  * All right reserved.
+  * Function：String Utils for Scala Test
+  *
+  * @author Benedict Jin
+  * @since 2018/5/30
+  */
+class StrUtils4ScalaTest extends UnitTestStyle {
+
+  "Put a string and sub string into countSubString method" should "output a count of sub string" in {
+    countSubString("", "%s") should be(0)
+    countSubString("%s_%s", "%s") should be(2)
+    countSubString("%s_%s%S", "%s") should be(2)
+    countSubString("%s_%s%S%s", "%s") should be(3)
+  }
+
+  "Put a string and sub string into patchSubString method" should "output a count of sub string" in {
+    patchSubString("", "%s") should be("")
+    patchSubString("%s_%s", "%s") should be("%s_")
+    patchSubString("%s_%s%S", "%s") should be("%s_%S")
+    patchSubString("%s_%s%S%s", "%s") should be("%s_%s%S")
+  }
+
+  "Put few params into superFormat method" should "output a formatted string" in {
+    superFormat("%s_%s", "A") should be("A_")
+    superFormat("%s_%s", "A", "B") should be("A_B")
+    superFormat("%s_%s", "A", "B", "C") should be("A_B")
+  }
+}
