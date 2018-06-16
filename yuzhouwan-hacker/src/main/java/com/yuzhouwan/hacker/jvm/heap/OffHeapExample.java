dc055a7fb31e621fbc4a62861f7dba87b53f29a1
asdf2014 <benedictjin2016@gmail.com>
2018-06-16 16:35:25 +0800

    Add off-heap example

---
+++ yuzhouwan-hacker/src/main/java/com/yuzhouwan/hacker/jvm/heap/OffHeapExample.java
@@ -0,0 +1,35 @@
+package com.yuzhouwan.hacker.jvm.heap;
+
+import sun.misc.VM;
+import sun.nio.ch.DirectBuffer;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Copyright @ 2018 yuzhouwan.com
+ * All right reserved.
+ * Function：Off-Heap Example
+ *
+ * @author Benedict Jin
+ * @since 2018/6/15
+ */
+public class OffHeapExample {
+
+    /*
+    -XX:MaxDirectMemorySize=64M 可以控制堆外内存大小，默认在 VM 静态变量 directMemory 为 64M
+
+    maxDirectMemory: 67108864
+    isDirect: true
+     */
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("maxDirectMemory: " + VM.maxDirectMemory());
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1024 * 1024 * 64);
+        Thread.sleep(200);
+        boolean isDirect = buffer.isDirect();
+        System.out.println("isDirect: " + isDirect);
+        if (isDirect) ((DirectBuffer) buffer).cleaner().clean();
+        else buffer.clear();
+        Thread.sleep(200);
+        System.exit(0);
+    }
+}
