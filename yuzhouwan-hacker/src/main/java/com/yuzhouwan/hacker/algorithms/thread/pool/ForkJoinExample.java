06e90da6ae4bfb87e336111f4c805f0973da14cb
asdf2014 <benedictjin2016@gmail.com>
2018-07-12 17:41:55 +0800

    Add testcase for ForkJoin

---
+++ yuzhouwan-hacker/src/main/java/com/yuzhouwan/hacker/algorithms/thread/pool/ForkJoinExample.java
@@ -0,0 +1,57 @@
+package com.yuzhouwan.hacker.algorithms.thread.pool;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+
+/**
+ * Copyright @ 2018 yuzhouwan.com
+ * All right reserved.
+ * Function：Fork Join Example
+ *
+ * @author Benedict Jin
+ * @since 2018/7/11
+ */
+public class ForkJoinExample {
+
+    public static void main(String[] args) {
+
+        ForkJoinPool.commonPool().getParallelism();
+        ForkJoinPool pool = new ForkJoinPool();
+        new Thread() {
+            @Override
+            public void run() {
+                pool.submit(() -> System.out.println("Hello,world first"));
+
+                ForkJoinTask<String> task = pool.submit(() -> "Hello,world second");
+                // 输出 Hello,world（永远不会输出，也不会报异常， 所以这是个bug）
+                try {
+                    System.out.println(task.get());
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            {
+                this.setName("printHelloWorld");
+                this.start();
+            }
+        };
+        /*try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        new Thread() {
+            @Override
+            public void run() {
+                pool.shutdown();
+            }
+
+            {
+                this.setName("shutdownPool");
+                this.start();
+            }
+        };
+    }
+}
