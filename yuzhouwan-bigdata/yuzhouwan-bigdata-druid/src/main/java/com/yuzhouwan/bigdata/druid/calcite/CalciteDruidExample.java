3abb51e1d75873c795e0caf514a4620d8ccf6655
asdf2014 <asdf2014@apache.org>
2020-07-13 00:12:18 +0800

    Add an example of calcite druid adaptar

---
+++ yuzhouwan-bigdata/yuzhouwan-bigdata-druid/src/main/java/com/yuzhouwan/bigdata/druid/calcite/CalciteDruidExample.java
@@ -0,0 +1,31 @@
+package com.yuzhouwan.bigdata.druid.calcite;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Copyright @ 2020 yuzhouwan.com
+ * All right reserved.
+ * Function：Calcite Druid Example
+ *
+ * @author Benedict Jin
+ * @since 2020/7/12
+ */
+public class CalciteDruidExample {
+
+    @SuppressWarnings("SqlResolve")
+    public static void main(String[] args) throws Exception {
+        String url = "jdbc:avatica:remote:url=http://localhost:8082/druid/v2/sql/avatica/";
+        String query = "SELECT COUNT(*) as res FROM wikipedia";
+        try (Connection connection = DriverManager.getConnection(url)) {
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(query)) {
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getInt(1));
+                }
+            }
+        }
+    }
+}
