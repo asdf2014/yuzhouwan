2b2a83c4d27bbe4450d82ab068e3973d83e5dee5
asdf2014 <asdf2014@apache.org>
2022-08-22 23:32:04 +0800

    Add code comments and running results for the example of go-druid

--- yuzhouwan-hacker/src/main/golang/druid/main.go
+++ yuzhouwan-hacker/src/main/golang/druid/main.go
@@ -20,29 +20,37 @@ import (
 // Copied from https://github.com/grafadruid/go-druid/blob/master/examples/main.go
 
 func main() {
+
+	// Connection
 	var druidOpts []druid.ClientOption
 	druidOpts = append(druidOpts, druid.WithSkipTLSVerify())
-
 	d, err := druid.NewClient("http://localhost:8082", druidOpts...)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Version
 	status, _, err := d.Common().Status()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("{\"version\": \"" + status.Version + "\"}")
+	fmt.Println("{\"version\": \"" + status.Version + "\"}") // {"version": "0.22.0"}
 
+	// DataSource
 	t := datasource.NewTable().SetName("wikipedia")
 
+	// Time range
 	i := intervals.NewInterval()
 	i.SetInterval(time.Unix(0, 0), time.Now())
 	i2 := intervals.NewInterval()
 	i2.SetIntervalWithString("2021-01-21T14:59:05.000Z", "P1D")
 	is := intervals.NewIntervals().SetIntervals([]*intervals.Interval{i, i2})
 
+	// Aggregation
 	c := aggregation.NewCount().SetName("count")
 	aa := []builder.Aggregator{c}
+
+	// Columns
 	s1 := filter.NewSelector().SetDimension("countryName").SetValue("France")
 	s2 := filter.NewSelector().SetDimension("cityName").SetValue("Paris")
 	n := filter.NewNot().SetField(s2)
@@ -50,10 +58,23 @@ func main() {
 	m := granularity.NewSimple().SetGranularity(granularity.All)
 	ts := query.NewTimeseries().SetDataSource(t).SetIntervals(is).SetAggregations(aa).SetGranularity(m).SetFilter(a).SetLimit(10)
 	var results interface{}
+
+	// Execute
 	_, err = d.Query().Execute(ts, &results)
 	if err != nil {
 		log.Fatalf("Execute failed, %s", err)
 	}
 
+	// Result
+	/**
+	([]interface {}) (len=1 cap=1) {
+	 (map[string]interface {}) (len=2) {
+	  (string) (len=9) "timestamp": (string) (len=24) "2016-06-27T00:00:11.080Z",
+	  (string) (len=6) "result": (map[string]interface {}) (len=1) {
+	   (string) (len=5) "count": (float64) 79
+	  }
+	 }
+	}
+	*/
 	spew.Dump(results)
 }
