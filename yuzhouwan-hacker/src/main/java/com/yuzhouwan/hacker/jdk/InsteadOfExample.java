4d8c6de61f94e6f696cbedae94d15f7eacad6c4d
asdf2014 <asdf2014@apache.org>
2023-08-13 21:04:30 +0800

    Add a example for new instead of

---
+++ yuzhouwan-hacker/src/main/java/com/yuzhouwan/hacker/jdk/InsteadOfExample.java
@@ -0,0 +1,31 @@
+package com.yuzhouwan.hacker.jdk;
+
+/**
+ * Copyright @ 2023 yuzhouwan.com
+ * All right reserved.
+ * Function：Instead of Example
+ *
+ * @author Benedict Jin
+ * @since 2023/8/9
+ */
+@SuppressWarnings({"ConstantConditions",
+    "PatternVariableCanBeUsed",
+    "CastCanBeRemovedNarrowingVariableType"})
+public class InsteadOfExample {
+
+  public static void main(String[] args) {
+
+    Object obj = "yuzhouwan";
+
+    // Old code
+    if (obj instanceof String) {
+      String s = (String) obj;
+      System.out.println(s);
+    }
+
+    // New code
+    if (obj instanceof String s) {
+      System.out.println(s);
+    }
+  }
+}
