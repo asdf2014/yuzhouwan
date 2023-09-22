0f6fb39fd3ad1897392619bb0984e1f8e6da313d
asdf2014 <asdf2014@apache.org>
2023-09-22 18:00:49 +0800

    Improve LoadThread

--- yuzhouwan-hacker/src/main/java/com/yuzhouwan/hacker/jvm/gc/LoadThread.java
+++ yuzhouwan-hacker/src/main/java/com/yuzhouwan/hacker/jvm/gc/LoadThread.java
@@ -18,16 +18,21 @@ class LoadThread extends Thread {
     private static final long TIME_ZERO = System.currentTimeMillis();
     private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
 
+    @Override
     public void run() {
         AbstractQueue<String> queue = new ArrayBlockingQueue<>(GCTest.countDownSize);
         for (int i = 0, finishedUnit = 0; ; i = i + 1) {
             // Simulate object use to force promotion into OldGen and then GC
             if (queue.size() >= GCTest.countDownSize) {
-                for (int j = 0; j < GCTest.eachRemoveSize; j++) queue.remove();
+                for (int j = 0; j < GCTest.eachRemoveSize; j++) {
+                    queue.remove();
+                }
                 finishedUnit++;
 
                 // every 1000 removal is counted as 1 unit.
-                if (System.currentTimeMillis() - TIME_ZERO > GCTest.duration * 1000) System.exit(0);
+                if (System.currentTimeMillis() - TIME_ZERO > GCTest.duration * 1000L) {
+                    System.exit(0);
+                }
                 System.out.println(SIMPLE_DATE_FORMAT.format(new Date()) + " finished Units (1K) = " + finishedUnit);
             }
             queue.add(new String(new char[GCTest.referenceSize]).replace('\0', 'a'));
