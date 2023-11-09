4274a515272f8548077c986aab85d4260950d8b9
asdf2014 <asdf2014@apache.org>
2023-11-09 22:08:51 +0800

    Use org.junit.Assert.assertNotNull instead of org.springframework.util.Assert

--- yuzhouwan-common/src/test/java/com/yuzhouwan/common/util/IpUtilsTest.java
+++ yuzhouwan-common/src/test/java/com/yuzhouwan/common/util/IpUtilsTest.java
@@ -4,7 +4,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -24,6 +23,7 @@ import static com.yuzhouwan.common.util.IpUtils.ping;
 import static com.yuzhouwan.common.util.IpUtils.removeTail32;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -157,7 +157,7 @@ public class IpUtilsTest {
   @Test
   public void checkIP2IntTest() {
     Integer ip2int = ip2int("10.1.1.0");
-    Assert.notNull(ip2int);
+    assertNotNull(ip2int);
     int ip = ip2int;
     assertEquals(167837952, ip);
     assertEquals(0x0A010100, ip);
